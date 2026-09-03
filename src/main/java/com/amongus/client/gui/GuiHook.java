183dd5d60ad4e1f277919db6ca6bc9032597ef09
raindotz <168026138+raindotz@users.noreply.github.com>
2026-09-03 17:00:04 +0330

    Update GuiHook.java

--- src/main/java/com/amongus/client/gui/GuiHook.java
+++ src/main/java/com/amongus/client/gui/GuiHook.java
@@ -4,10 +4,7 @@ import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-@SideOnly(Side.CLIENT)
 public class GuiHook {
     public GuiHook() {
         MinecraftForge.EVENT_BUS.register(this);

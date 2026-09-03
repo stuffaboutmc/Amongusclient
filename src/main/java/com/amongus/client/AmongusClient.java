c4de0f29a019228d40978b79c4a9b5806ac50d34
raindotz <168026138+raindotz@users.noreply.github.com>
2026-09-03 23:16:28 +0330

    Update AmongusClient.java

--- src/main/java/com/amongus/client/AmongusClient.java
+++ src/main/java/com/amongus/client/AmongusClient.java
@@ -1,10 +1,13 @@
 package com.amongus.client;
 
+import com.amongus.client.utils.ConfigManager;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import org.lwjgl.input.Keyboard;
 
 @Mod(modid = "amongus", name = "Amongus Client", version = "1.0")
@@ -20,11 +23,17 @@ public class AmongusClient {
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(moduleManager);
         MinecraftForge.EVENT_BUS.register(commandManager);
+        ConfigManager.loadConfig(); // auto-load
         System.out.println("[Amongus] Initialized");
     }
 
     @SubscribeEvent
     public void onKeyInput(InputEvent.KeyInputEvent event) {
         if (Keyboard.getEventKeyState()) moduleManager.handleKey(Keyboard.getEventKey());
+    }
+
+    @SubscribeEvent
+    public void onPlayerLogout(PlayerEvent.PlayerLoggedOutEvent event) {
+        ConfigManager.saveConfig();
     }
 }

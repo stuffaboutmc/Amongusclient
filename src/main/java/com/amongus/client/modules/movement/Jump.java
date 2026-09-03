824c35426bb858c2d5f349179e45bfab0ddb08cb
raindotz <168026138+raindotz@users.noreply.github.com>
2026-09-03 16:12:39 +0330

    Update Jump.java

--- src/main/java/com/amongus/client/modules/movement/Jump.java
+++ src/main/java/com/amongus/client/modules/movement/Jump.java
@@ -7,6 +7,7 @@ import org.lwjgl.input.Keyboard;
 
 public class Jump extends Module {
     private int tickCounter = 0;
+
     public Jump() {
         super("Jump", Keyboard.KEY_NONE, Category.MOVEMENT, "Jumps every X ticks.");
         addSetting(new Setting("Enabled", new String[]{"Off", "On"}, "Off"));
@@ -15,10 +16,13 @@ public class Jump extends Module {
 
     @SubscribeEvent
     public void onLivingUpdate(LivingEvent.LivingUpdateEvent event) {
-        if (event.entity = mc.thePlayer) return;
+        if (event.entity != mc.thePlayer) return;
         if (getSetting("Enabled").getValue().equals("Off")) return;
         tickCounter++;
         int interval = (int) getSetting("Interval").getDoubleValue();
-        if (tickCounter >= interval) { if (mc.thePlayer.onGround) mc.thePlayer.jump(); tickCounter = 0; }
+        if (tickCounter >= interval) {
+            if (mc.thePlayer.onGround) mc.thePlayer.jump();
+            tickCounter = 0;
+        }
     }
 }

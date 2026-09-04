901cd46adcc35495f1a002441dd3fe80b8659d50
raindotz <168026138+raindotz@users.noreply.github.com>
2026-09-05 00:23:47 +0330

    Delete src/main directory

--- src/main/java/com/amongus/client/modules/misc/DiscordRPC.java
+++
@@ -1,9 +0,0 @@
-package com.amongus.client.modules.misc;
-import com.amongus.client.modules.Module;
-import org.lwjgl.input.Keyboard;
-public class DiscordRPC extends Module {
-    public DiscordRPC() {
-        super("DiscordRPC", Keyboard.KEY_NONE, Category.MISC, "Shows Discord rich presence.");
-        addSetting(new Setting("Mode", new String[]{"None","Basic","Advanced"}, "Basic"));
-    }
-}
--- src/main/java/com/amongus/client/modules/misc/NoRotate.java
+++
@@ -1,9 +0,0 @@
-package com.amongus.client.modules.misc;
-import com.amongus.client.modules.Module;
-import org.lwjgl.input.Keyboard;
-public class NoRotate extends Module {
-    public NoRotate() {
-        super("NoRotate", Keyboard.KEY_NONE, Category.MISC, "Server can't rotate you.");
-        addSetting(new Setting("Mode", new String[]{"None","Basic","Advanced"}, "Basic"));
-    }
-}

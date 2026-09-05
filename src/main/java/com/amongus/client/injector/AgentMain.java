409e9bf611c9e2cd99cbf5b5ee9f9a0974a836e2
raindotz <168026138+raindotz@users.noreply.github.com>
2026-09-05 09:31:03 +0330

    Delete src/main/java/com/amongus/client/injector directory

--- src/main/java/com/amongus/client/injector/AgentMain.java
+++
@@ -1,6 +0,0 @@
-package com.amongus.client.injector; 
-import java.lang.instrument.Instrumentation; 
-public class AgentMain { 
-    public static void premain(String args, Instrumentation inst) {} 
-    public static void agentmain(String args, Instrumentation inst) { premain(args, inst); } 
-} 

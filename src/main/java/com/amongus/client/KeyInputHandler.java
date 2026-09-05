13d91a0c95213dcd77a60e5074511ec880bb73e1
raindotz <168026138+raindotz@users.noreply.github.com>
2026-09-05 11:18:37 +0330

    Update KeyInputHandler.java

--- src/main/java/com/amongus/client/KeyInputHandler.java
+++ src/main/java/com/amongus/client/KeyInputHandler.java
@@ -1,6 +1,7 @@
 package com.amongus.client;
 
 import com.amongus.client.modules.ClickGUIModule;
+import com.amongus.client.modules.Module;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import org.lwjgl.input.Keyboard;
@@ -9,11 +10,10 @@ public class KeyInputHandler {
 
     @SubscribeEvent
     public void onKeyInput(InputEvent.KeyInputEvent event) {
-        // Check if the ClickGUI key (Right Shift, keycode 54) is pressed
-        if (Keyboard.isKeyDown(54)) {
+        if (Keyboard.isKeyDown(54)) { // Right Shift
             for (Module mod : ModuleManager.modules) {
                 if (mod instanceof ClickGUIModule) {
-                    mod.toggle(); // opens the GUI via onEnable()
+                    mod.toggle();
                     break;
                 }
             }

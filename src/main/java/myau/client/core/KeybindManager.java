373375239f6049daffb56c9b812db655b7bb32d9
raindotz <168026138+raindotz@users.noreply.github.com>
2026-09-05 23:02:04 +0330

    Update KeybindManager.java

--- src/main/java/myau/client/core/KeybindManager.java
+++ src/main/java/myau/client/core/KeybindManager.java
@@ -1,36 +1,35 @@
-package myau.client.core;
+package com.stuffaboutmc.client.manager;
 
-import java.util.*;
+import com.stuffaboutmc.client.Client;
+import com.stuffaboutmc.client.gui.ClickGUI;
 import org.lwjgl.input.Keyboard;
 
 public class KeybindManager {
-    private static final int GUI_KEY = Keyboard.KEY_RSHIFT;
-    private static final Map<Integer, Boolean> prevStates = new HashMap<>();
 
-    public static void init() { prevStates.clear(); }
-
-    public static void update() {
-        for (Module m : ModuleManager.getModules()) {
-            int key = m.getKey();
-            if (key == 0 || key == GUI_KEY) continue;
-            boolean down = Keyboard.isKeyDown(key);
-            Boolean was = prevStates.get(key);
-            if (was == null) was = false;
-            if (down && !was) m.toggle();
-            prevStates.put(key, down);
+    public void onTick() {
+        // RSHIFT toggle clickgui
+        if (Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
+            if (ClickGUI.instance != null) {
+                ClickGUI.instance.toggleVisibility();
+            } else {
+                ClickGUI gui = new ClickGUI();
+                Minecraft.getMinecraft().displayGuiScreen(gui);
+            }
+            // prevent rapid toggling
+            try { Thread.sleep(100); } catch (InterruptedException ignored) {}
         }
-    }
 
-    public static void setKeybind(Module m, int key) {
-        if (m == null || key == GUI_KEY) return;
-        for (Module other : ModuleManager.getModules()) {
-            if (other != m && other.getKey() == key && key != 0) other.setKey(0);
+        // module keybinds
+        for (Module m : Client.instance.moduleManager.getModules()) {
+            if (m.getKeybind() > 0 && Keyboard.isKeyDown(m.getKeybind())) {
+                // prevent spam
+                if (!m.wasPressed) {
+                    m.toggle();
+                    m.wasPressed = true;
+                }
+            } else {
+                m.wasPressed = false;
+            }
         }
-        m.setKey(key);
-    }
-
-    public static String getKeyName(int key) {
-        if (key == 0) return "None";
-        return Keyboard.getKeyName(key);
     }
 }

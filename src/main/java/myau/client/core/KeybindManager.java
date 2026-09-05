6918d1f63396c277159b8b8740f01f2e884e6514
raindotz <168026138+raindotz@users.noreply.github.com>
2026-09-05 23:59:20 +0330

    Update KeybindManager.java

--- src/main/java/myau/client/core/KeybindManager.java
+++ src/main/java/myau/client/core/KeybindManager.java
@@ -1,51 +1,42 @@
 package myau.client.core;
 
 import myau.client.module.Module;
-import myau.client.module.impl.Flight;
-import myau.client.module.impl.Speed;
-import myau.client.module.impl.Sprint;
-import myau.client.module.impl.FullBright;
-import myau.client.module.impl.KillAura;
+import myau.client.gui.ClickGUI;
+import net.minecraft.client.Minecraft;
+import org.lwjgl.input.Keyboard;
 
-import java.util.ArrayList;
-import java.util.List;
+public class KeybindManager {
 
-public class ModuleManager {
+    private static long lastToggle = 0;
 
-    private static List<Module> modules = new ArrayList<>();
+    public static void init() {}
 
-    public static void init() {
-        modules.add(new Flight());
-        modules.add(new Speed());
-        modules.add(new Sprint());
-        modules.add(new FullBright());
-        modules.add(new KillAura());
-    }
-
-    public static List<Module> getModules() { return modules; }
-
-    public static Module getModule(String name) {
-        for (Module m : modules) {
-            if (m.getName().equalsIgnoreCase(name)) return m;
+    public static void update() {
+        if (Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
+            long now = System.currentTimeMillis();
+            if (now - lastToggle > 200) {
+                lastToggle = now;
+                if (Minecraft.getMinecraft().currentScreen instanceof ClickGUI) {
+                    Minecraft.getMinecraft().displayGuiScreen(null);
+                } else {
+                    Minecraft.getMinecraft().displayGuiScreen(new ClickGUI());
+                }
+            }
         }
-        return null;
-    }
 
-    public static void onTick() {
-        for (Module m : modules) {
-            if (m.isEnabled()) m.onTick();
-        }
-    }
-
-    public static void onUpdate() {
-        for (Module m : modules) {
-            if (m.isEnabled()) m.onUpdate();
+        for (Module m : ModuleManager.getModules()) {
+            if (m.getKeybind() > 0 && Keyboard.isKeyDown(m.getKeybind())) {
+                if (!m.wasPressed) {
+                    m.toggle();
+                    m.wasPressed = true;
+                }
+            } else {
+                m.wasPressed = false;
+            }
         }
     }
 
-    public static void onRender2D(float partialTicks) {
-        for (Module m : modules) {
-            if (m.isEnabled()) m.onRender2D(partialTicks);
-        }
+    public static void setKeybind(Module m, int key) {
+        if (m != null) m.setKeybind(key);
     }
 }

55704010ee244bf23a03c3d02de0926556eca85b
raindotz <168026138+raindotz@users.noreply.github.com>
2026-09-05 23:59:36 +0330

    Update ModuleManager.java

--- src/main/java/myau/client/core/ModuleManager.java
+++ src/main/java/myau/client/core/ModuleManager.java
@@ -1,31 +1,51 @@
-package com.stuffaboutmc.client.manager;
+package myau.client.core;
 
-import com.stuffaboutmc.client.module.Module;
-import com.stuffaboutmc.client.module.impl.Flight;
-import com.stuffaboutmc.client.module.impl.Speed;
-import com.stuffaboutmc.client.module.impl.KillAura;
-import com.stuffaboutmc.client.module.impl.Sprint;
-import com.stuffaboutmc.client.module.impl.FullBright;
+import myau.client.module.Module;
+import myau.client.module.impl.Flight;
+import myau.client.module.impl.Speed;
+import myau.client.module.impl.Sprint;
+import myau.client.module.impl.FullBright;
+import myau.client.module.impl.KillAura;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ModuleManager {
-    private List<Module> modules = new ArrayList<>();
 
-    public ModuleManager() {
+    private static List<Module> modules = new ArrayList<>();
+
+    public static void init() {
         modules.add(new Flight());
         modules.add(new Speed());
-        modules.add(new KillAura());
         modules.add(new Sprint());
         modules.add(new FullBright());
+        modules.add(new KillAura());
     }
 
-    public List<Module> getModules() { return modules; }
-    public Module getModuleByName(String name) {
+    public static List<Module> getModules() { return modules; }
+
+    public static Module getModule(String name) {
         for (Module m : modules) {
             if (m.getName().equalsIgnoreCase(name)) return m;
         }
         return null;
+    }
+
+    public static void onTick() {
+        for (Module m : modules) {
+            if (m.isEnabled()) m.onTick();
+        }
+    }
+
+    public static void onUpdate() {
+        for (Module m : modules) {
+            if (m.isEnabled()) m.onUpdate();
+        }
+    }
+
+    public static void onRender2D(float partialTicks) {
+        for (Module m : modules) {
+            if (m.isEnabled()) m.onRender2D(partialTicks);
+        }
     }
 }

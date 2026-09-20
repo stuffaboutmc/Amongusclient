1f306b22304349dd42ba2af172021cffce0ea8ad
Saphira0310 <270860030+Saphira0310@users.noreply.github.com>
2026-09-20 10:17:40 +0000

    IDK

--- build/sources/main/java/myau/client/command/Command.java
+++
@@ -1,7 +0,0 @@
-package myau.client.command;
-public abstract class Command {
-    private final String name;
-    public Command(String name) { this.name = name; }
-    public String getName() { return name; }
-    public abstract void execute(String[] args);
-}
